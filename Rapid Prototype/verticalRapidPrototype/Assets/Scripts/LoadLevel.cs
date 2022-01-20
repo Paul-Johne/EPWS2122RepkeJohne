23b76a88a48aef5e5c0add930348287f7cff182e
Paul André Johne (inf1984) <paul_andre.johne@smail.th-koeln.de>
2022-01-21 00:38:48 +0100

    improved loading script

--- Rapid Prototype/verticalRapidPrototype/Assets/Scripts/LoadLevel.cs
+++ Rapid Prototype/verticalRapidPrototype/Assets/Scripts/LoadLevel.cs
@@ -46,18 +46,25 @@ public class LoadLevel : MonoBehaviour {
             throw new Exception("LoadingScene<T>() only accepts string or int");
         }
 
+        /* Callback wenn Szene geladen ist */
+        asyncLoadScene.completed += (AsyncOperation it) => {
+            Debug.Log("Fertig");
+            transition.SetTrigger("sceneLoaded");
+        };
+
         asyncLoadScene.allowSceneActivation = false;
 
-        yield return new WaitForSecondsRealtime(2f); // temporary solution
-        asyncLoadScene.allowSceneActivation = true;
-
-        while (!asyncLoadScene.isDone) {
+        /* hier warten auf Animation fertig geladen */
+        while (!(transition.GetCurrentAnimatorStateInfo(0).IsName("WipeFade_Start") && transition.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 - 1e-6)) {
             yield return null;
         }
 
-        transition.SetTrigger("sceneLoaded");
+        asyncLoadScene.allowSceneActivation = true;
 
-        yield return new WaitForSecondsRealtime(2f); // temporary solution
+        while (!(transition.GetCurrentAnimatorStateInfo(0).IsName("WipeFade_End") && transition.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 - 1e-6)) {
+            yield return null;
+        }
+
         Destroy(gameObject);
     }
 }
